babfe8bc2a4dbe390e6afbc5fd8a7e1d28caae66
Gerhard P <github@gecomp.at>
2026-07-18 15:36:58 +0200

    feat: support numeric comma and semicolon delimiters

--- Code/01_Common/085_TVF_ParseBigintList.sql
+++ Code/01_Common/085_TVF_ParseBigintList.sql
@@ -4,14 +4,18 @@ GO
 /*
 ===============================================================================
 Objekt       : monitor.TVF_ParseBigintList
-Version      : 1.0.0
-Stand        : 2026-07-15
+Version      : 1.1.0
+Stand        : 2026-07-18
 Typ          : Inline Table-valued Function
-Zweck        : Validiert eine Pipe-Liste ganzzahliger bigint-Werte. Diese
-               Funktion ist für Session-, Query-, Index- und andere numerische
-               ID-Listen vorgesehen und verwendet bewusst keinen Identifier-
-               Parser.
+Zweck        : Validiert eine mit Pipe, Beistrich oder Strichpunkt getrennte
+               Liste ganzzahliger bigint-Werte. Die Trennzeichen dürfen gemischt
+               verwendet werden. Diese Funktion ist für Session-, Query-,
+               Index- und andere numerische ID-Listen vorgesehen und verwendet
+               bewusst keinen Identifier-Parser.
 SQL-Version  : SQL Server 2019 oder neuer.
+Beispiele    : SELECT * FROM monitor.TVF_ParseBigintList(N'57|61');
+               SELECT * FROM monitor.TVF_ParseBigintList(N'57, 61;72');
+Aenderungen  : 1.1.0 - Beistrich und Strichpunkt als weitere Trennzeichen.
 ===============================================================================
 */
 CREATE OR ALTER FUNCTION [monitor].[TVF_ParseBigintList]
@@ -38,6 +42,7 @@ RETURN
               CASE WHEN [l].[IsValid] = 0 THEN [l].[ErrorMessage]
                    WHEN TRY_CONVERT(bigint, LTRIM(RTRIM([l].[ItemText]))) IS NULL
                    THEN N'Das Listenelement ist keine gültige bigint-Ganzzahl.' END)
-    FROM [monitor].[TVF_ParsePipeList](@List) AS [l]
+    FROM [monitor].[TVF_ParsePipeList]
+         (TRANSLATE(@List, N',;', N'||')) AS [l]
 );
 GO
