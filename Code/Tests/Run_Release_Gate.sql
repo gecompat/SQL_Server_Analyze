17627dd54fdd4adc3a136c7927731e6a81f50d3e
Gerhard P <github@gecomp.at>
2026-07-18 13:21:44 +0200

    test: include P1 statistics suite in release gate

--- Code/Tests/Run_Release_Gate.sql
+++ Code/Tests/Run_Release_Gate.sql
@@ -17,65 +17,68 @@ Datenschutz  : Laufzeitausgaben werden nicht in Dateien geschrieben. Eine
 ===============================================================================
 */
 
-RAISERROR(N'RELEASE_GATE 1/19: Smoke Test',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 1/20: Smoke Test',10,1) WITH NOWAIT;
 :r Integration/110_Smoke_Test.sql
 
-RAISERROR(N'RELEASE_GATE 2/19: Parameter-API-Vertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 2/20: Parameter-API-Vertrag',10,1) WITH NOWAIT;
 :r Integration/163_Parameter_API_Vertrag.sql
 
-RAISERROR(N'RELEASE_GATE 3/19: Filter- und Ausgabe-Vertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 3/20: Filter- und Ausgabe-Vertrag',10,1) WITH NOWAIT;
 :r Integration/165_Filter_Output_Contract.sql
 
-RAISERROR(N'RELEASE_GATE 4/19: Spezialfall-API-Vertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 4/20: Spezialfall-API-Vertrag',10,1) WITH NOWAIT;
 :r Integration/167_Special_Case_API_Contract.sql
 
-RAISERROR(N'RELEASE_GATE 5/19: Spezialfall-Laufzeitvertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 5/20: Spezialfall-Laufzeitvertrag',10,1) WITH NOWAIT;
 :r Integration/168_Special_Case_Runtime_Contract.sql
 
-RAISERROR(N'RELEASE_GATE 6/19: P0-Laufzeitvertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 6/20: P0-Laufzeitvertrag',10,1) WITH NOWAIT;
 :r Integration/169_P0_Runtime_Contract.sql
 
-RAISERROR(N'RELEASE_GATE 7/19: P1-IQP-Laufzeitvertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 7/20: P1-IQP-Laufzeitvertrag',10,1) WITH NOWAIT;
 :r Integration/170_P1_IQP_Runtime_Contract.sql
 
-RAISERROR(N'RELEASE_GATE 8/19: P1-Contention-Laufzeitvertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 8/20: P1-Contention-Laufzeitvertrag',10,1) WITH NOWAIT;
 :r Integration/171_P1_Contention_Runtime_Contract.sql
 
-RAISERROR(N'RELEASE_GATE 9/19: P1-Speicher-Laufzeitvertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 9/20: P1-Speicher-Laufzeitvertrag',10,1) WITH NOWAIT;
 :r Integration/172_P1_Memory_Runtime_Contract.sql
 
-RAISERROR(N'RELEASE_GATE 10/19: P1-Backupketten-Laufzeitvertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 10/20: P1-Backupketten-Laufzeitvertrag',10,1) WITH NOWAIT;
 :r Integration/173_P1_Backup_Runtime_Contract.sql
 
-RAISERROR(N'RELEASE_GATE 11/19: P1-Schema-Laufzeitvertrag',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 11/20: P1-Schema-Laufzeitvertrag',10,1) WITH NOWAIT;
 :r Integration/174_P1_Schema_Runtime_Contract.sql
 
-RAISERROR(N'RELEASE_GATE 12/19: Common',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 12/20: P1-Statistikverteilungs-Laufzeitvertrag',10,1) WITH NOWAIT;
+:r Integration/175_P1_Statistics_Runtime_Contract.sql
+
+RAISERROR(N'RELEASE_GATE 13/20: Common',10,1) WITH NOWAIT;
 :r Common/090_Test_und_Abnahme_Phase1A.sql
 
-RAISERROR(N'RELEASE_GATE 13/19: Current State',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 14/20: Current State',10,1) WITH NOWAIT;
 :r CurrentState/110_Test_und_Abnahme_Phase1B.sql
 
-RAISERROR(N'RELEASE_GATE 14/19: Object und Index',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 15/20: Object und Index',10,1) WITH NOWAIT;
 :r ObjectIndex/110_Test_und_Abnahme_Phase2.sql
 
-RAISERROR(N'RELEASE_GATE 15/19: Plan Cache',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 16/20: Plan Cache',10,1) WITH NOWAIT;
 :r PlanCache/110_Test_und_Abnahme_Phase3.sql
 
-RAISERROR(N'RELEASE_GATE 16/19: Query Store',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 17/20: Query Store',10,1) WITH NOWAIT;
 :r QueryStore/110_Test_und_Abnahme_Phase4.sql
 
-RAISERROR(N'RELEASE_GATE 17/19: Extended Events',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 18/20: Extended Events',10,1) WITH NOWAIT;
 :r ExtendedEvents/110_Test_und_Abnahme_Phase5.sql
 
-RAISERROR(N'RELEASE_GATE 18/19: Infrastructure',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 19/20: Infrastructure',10,1) WITH NOWAIT;
 :r Infrastructure/110_Test_und_Abnahme_Phase6.sql
 
-RAISERROR(N'RELEASE_GATE 19/19: Server Health',10,1) WITH NOWAIT;
+RAISERROR(N'RELEASE_GATE 20/20: Server Health',10,1) WITH NOWAIT;
 :r ServerHealth/110_Test_und_Abnahme_Phase7.sql
 
 SELECT CAST('AVAILABLE' AS varchar(40)) AS [StatusCode],
        CAST(0 AS bit) AS [IsPartial],
-       CAST(19 AS int) AS [ExecutedSuites],
+       CAST(20 AS int) AS [ExecutedSuites],
        N'Alle Integrationsverträge und Bereichs-Smoke-Tests wurden ohne SQL-Fehler beendet.' AS [Detail];
 GO
