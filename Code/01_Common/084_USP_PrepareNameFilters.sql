e6154207bf2b80e7237ea6bd03caa1cd49f7ac1a
Gerhard P <github@gecomp.at>
2026-07-19 19:16:23 +0200

    fix: preserve name filter permissions

--- Code/01_Common/084_USP_PrepareNameFilters.sql
+++ Code/01_Common/084_USP_PrepareNameFilters.sql
@@ -46,6 +46,17 @@ BEGIN
     DECLARE @FilterTableQuoted nvarchar(258)=QUOTENAME(@FilterTable);
     DECLARE @Sql nvarchar(max);
 
+    CREATE TABLE [#PrepareNameFilters_Work]
+    (
+          [FilterType] varchar(20) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL
+        , [ItemOrdinal] int NOT NULL
+        , [NameValue] sysname COLLATE SQL_Latin1_General_CP1_CS_AS NULL
+        , [DatabaseName] sysname COLLATE SQL_Latin1_General_CP1_CS_AS NULL
+        , [SchemaName] sysname COLLATE SQL_Latin1_General_CP1_CS_AS NULL
+        , [ObjectName] sysname COLLATE SQL_Latin1_General_CP1_CS_AS NULL
+        , [IsValid] bit NOT NULL
+    );
+
     BEGIN TRY
         SET @Sql=N'SELECT TOP (0) [FilterType],[ItemOrdinal],[NameValue],[DatabaseName],[SchemaName],[ObjectName] FROM '+@FilterTableQuoted+N';';
         EXEC [sys].[sp_executesql] @Sql;
@@ -56,18 +67,6 @@ BEGIN
         RETURN;
     END CATCH;
 
-    IF (@SchemaNames IS NOT NULL AND EXISTS (SELECT 1 FROM [monitor].[TVF_ParseSqlNameList](@SchemaNames) WHERE [IsValid] = 0))
-       OR (@ObjectNames IS NOT NULL AND EXISTS (SELECT 1 FROM [monitor].[TVF_ParseSqlNameList](@ObjectNames) WHERE [IsValid] = 0))
-       OR (@FullObjectNames IS NOT NULL AND EXISTS (SELECT 1 FROM [monitor].[TVF_ParseFullObjectNameList](@FullObjectNames) WHERE [IsValid] = 0))
-       OR (@IndexNames IS NOT NULL AND EXISTS (SELECT 1 FROM [monitor].[TVF_ParseSqlNameList](@IndexNames) WHERE [IsValid] = 0))
-       OR (@StatisticsNames IS NOT NULL AND EXISTS (SELECT 1 FROM [monitor].[TVF_ParseSqlNameList](@StatisticsNames) WHERE [IsValid] = 0))
-       OR (@ColumnNames IS NOT NULL AND EXISTS (SELECT 1 FROM [monitor].[TVF_ParseSqlNameList](@ColumnNames) WHERE [IsValid] = 0))
-    BEGIN
-        SET @StatusCode = 'INVALID_PARAMETER';
-        SET @ErrorMessage = N'Mindestens eine Namensliste ist syntaktisch ungültig.';
-        RETURN;
-    END;
-
     IF @FullObjectNames IS NOT NULL
        AND (@SchemaNames IS NOT NULL OR @ObjectNames IS NOT NULL)
     BEGIN
@@ -76,56 +75,68 @@ BEGIN
         RETURN;
     END;
 
-    SET @Sql=N'INSERT '+@FilterTableQuoted+N' ([FilterType],[ItemOrdinal],[NameValue],[DatabaseName],[SchemaName],[ObjectName])
-    SELECT ''SCHEMA'',[ItemOrdinal],[NameValue],NULL,NULL,NULL
-    FROM [monitor].[TVF_ParseSqlNameList](@pSchemaNames) WHERE [IsValid] = 1
-    UNION ALL
-    SELECT ''OBJECT'',[ItemOrdinal],[NameValue],NULL,NULL,NULL
-    FROM [monitor].[TVF_ParseSqlNameList](@pObjectNames) WHERE [IsValid] = 1
-    UNION ALL
-    SELECT ''INDEX'',[ItemOrdinal],[NameValue],NULL,NULL,NULL
-    FROM [monitor].[TVF_ParseSqlNameList](@pIndexNames) WHERE [IsValid] = 1
-    UNION ALL
-    SELECT ''STATISTICS'',[ItemOrdinal],[NameValue],NULL,NULL,NULL
-    FROM [monitor].[TVF_ParseSqlNameList](@pStatisticsNames) WHERE [IsValid] = 1
-    UNION ALL
-    SELECT ''COLUMN'',[ItemOrdinal],[NameValue],NULL,NULL,NULL
-    FROM [monitor].[TVF_ParseSqlNameList](@pColumnNames) WHERE [IsValid] = 1;
+    BEGIN TRY
+        INSERT [#PrepareNameFilters_Work]
+        ([FilterType],[ItemOrdinal],[NameValue],[DatabaseName],[SchemaName],[ObjectName],[IsValid])
+        SELECT 'SCHEMA',[ItemOrdinal],[NameValue],NULL,NULL,NULL,[IsValid]
+        FROM [monitor].[TVF_ParseSqlNameList](@SchemaNames)
+        UNION ALL
+        SELECT 'OBJECT',[ItemOrdinal],[NameValue],NULL,NULL,NULL,[IsValid]
+        FROM [monitor].[TVF_ParseSqlNameList](@ObjectNames)
+        UNION ALL
+        SELECT 'INDEX',[ItemOrdinal],[NameValue],NULL,NULL,NULL,[IsValid]
+        FROM [monitor].[TVF_ParseSqlNameList](@IndexNames)
+        UNION ALL
+        SELECT 'STATISTICS',[ItemOrdinal],[NameValue],NULL,NULL,NULL,[IsValid]
+        FROM [monitor].[TVF_ParseSqlNameList](@StatisticsNames)
+        UNION ALL
+        SELECT 'COLUMN',[ItemOrdinal],[NameValue],NULL,NULL,NULL,[IsValid]
+        FROM [monitor].[TVF_ParseSqlNameList](@ColumnNames);
 
-    INSERT '+@FilterTableQuoted+N' ([FilterType],[ItemOrdinal],[NameValue],[DatabaseName],[SchemaName],[ObjectName])
-    SELECT ''FULL_OBJECT'',[ItemOrdinal],NULL,[DatabaseName],[SchemaName],[ObjectName]
-    FROM [monitor].[TVF_ParseFullObjectNameList](@pFullObjectNames)
-    WHERE [IsValid] = 1;';
+        INSERT [#PrepareNameFilters_Work]
+        ([FilterType],[ItemOrdinal],[NameValue],[DatabaseName],[SchemaName],[ObjectName],[IsValid])
+        SELECT 'FULL_OBJECT',[ItemOrdinal],NULL,[DatabaseName],[SchemaName],[ObjectName],[IsValid]
+        FROM [monitor].[TVF_ParseFullObjectNameList](@FullObjectNames);
+    END TRY
+    BEGIN CATCH
+        SET @StatusCode = CASE WHEN ERROR_NUMBER()=1222 THEN 'TIMEOUT'
+                               WHEN ERROR_NUMBER() IN(229,262,297,300,371,916) THEN 'DENIED_PERMISSION'
+                               ELSE 'ERROR_HANDLED' END;
+        SET @ErrorMessage = ERROR_MESSAGE();
+        RETURN;
+    END CATCH;
 
-    EXEC [sys].[sp_executesql]
-          @Sql
-        , N'@pSchemaNames nvarchar(max),@pObjectNames nvarchar(max),@pFullObjectNames nvarchar(max),@pIndexNames nvarchar(max),@pStatisticsNames nvarchar(max),@pColumnNames nvarchar(max)'
-        , @pSchemaNames=@SchemaNames
-        , @pObjectNames=@ObjectNames
-        , @pFullObjectNames=@FullObjectNames
-        , @pIndexNames=@IndexNames
-        , @pStatisticsNames=@StatisticsNames
-        , @pColumnNames=@ColumnNames;
+    IF EXISTS(SELECT 1 FROM [#PrepareNameFilters_Work] WHERE [IsValid]=0)
+    BEGIN
+        SET @StatusCode = 'INVALID_PARAMETER';
+        SET @ErrorMessage = N'Mindestens eine Namensliste ist syntaktisch ungültig.';
+        RETURN;
+    END;
 
     DECLARE @HasDuplicates bit=0;
-    SET @Sql=N'SELECT @pHasDuplicates=CONVERT(bit,CASE WHEN EXISTS
+    SELECT @HasDuplicates=CONVERT(bit,CASE WHEN EXISTS
     (
-        SELECT 1 FROM '+@FilterTableQuoted+N'
+        SELECT 1 FROM [#PrepareNameFilters_Work] WHERE [IsValid]=1
         GROUP BY [FilterType],
                  [NameValue] COLLATE SQL_Latin1_General_CP1_CS_AS,
                  [DatabaseName] COLLATE SQL_Latin1_General_CP1_CS_AS,
                  [SchemaName] COLLATE SQL_Latin1_General_CP1_CS_AS,
                  [ObjectName] COLLATE SQL_Latin1_General_CP1_CS_AS
         HAVING COUNT(*) > 1
-    ) THEN 1 ELSE 0 END);';
-    EXEC [sys].[sp_executesql] @Sql,N'@pHasDuplicates bit OUTPUT',@pHasDuplicates=@HasDuplicates OUTPUT;
+    ) THEN 1 ELSE 0 END);
 
     IF @HasDuplicates=1
     BEGIN
-        SET @Sql=N'DELETE FROM '+@FilterTableQuoted+N';';
-        EXEC [sys].[sp_executesql] @Sql;
         SET @StatusCode = 'INVALID_PARAMETER';
         SET @ErrorMessage = N'Eine Namensliste enthält case-sensitiv doppelte Werte.';
+        RETURN;
     END;
+
+    SET @Sql=N'INSERT '+@FilterTableQuoted+N'
+([FilterType],[ItemOrdinal],[NameValue],[DatabaseName],[SchemaName],[ObjectName])
+SELECT [FilterType],[ItemOrdinal],[NameValue],[DatabaseName],[SchemaName],[ObjectName]
+FROM [#PrepareNameFilters_Work]
+WHERE [IsValid]=1;';
+    EXEC [sys].[sp_executesql] @Sql;
 END;
 GO
